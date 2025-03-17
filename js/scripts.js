6f2410561f9fab679e4fa50bf9b02e9b1843c520
Júlio Reus <reus_julio@hotmail.com>
2025-03-17 20:08:52 -0300

    This library is a joke, enjoy

--- js/scripts.js
+++ js/scripts.js
@@ -214,6 +214,7 @@ async function loadSongs() {
         const response = await fetch('musicas.json');
         const songs = await response.json();
         displaySongs(songs);
+        //displayNewLibrary(songs);
         displayLibrarySongs(songs);
     } catch(error) {
         console.error('Erro ao carregar: ', error);
@@ -333,32 +334,88 @@ sidebarLibrary.addEventListener('click', () => {
 
 /*  LIBRARY     */
 
+/*
+async function displayNewLibrary(songs) {
+    const library = document.querySelector('.library');
+
+    songs.forEach(song => {
+        song = verifyFields(song);
+
+        try {
+            const audio = new Audio(song);
+
+            await new Promise((resolve, reject) => {
+                audio.addEventListener('loadedmetadata', () => {
+                    resolve();
+                });
+                audio.addEventListener('error', (error) => {
+                    reject(error);
+                });
+            });
+
+            const totalSeconds = audio.duration;
+            const formatedDuration = formatSongDuration(totalSeconds);
+
+            const divSong = document.createElement('div');
+            divSong.classList.add('songRow');
+            divSong.innerHTML = `
+            <img src="${song.cover}" alt="${song.name}">
+            <h3>${song.name}<br/></h3><p>${song.artist}</p>
+                <p>${formatedDuration}</p>
+        `;
+
+        library.appendChild(divSong);
+
+        } catch (error) {
+            console.error(`Erro ao carregar ${song}:`, error);
+
+        }
+    });
+}
+*/
+
 function displayLibrarySongs(songs) {
     const library = document.querySelector('.library');
 
     songs.forEach(song => {
         song = verifyFields(song);
 
-        const divSong = document.createElement('div');
-        divSong.classList.add("songRow");
-            divSong.innerHTML = `
-            <img src="${song.cover}" alt="${song.name}">
-            <h3>${song.name}<br/></h3><p>${song.artist}</p>
-        `;
-        
-        if (song.file !== '#') {
-            divSong.addEventListener('click', () => {
-                document.querySelectorAll('.songRow').forEach(i => i.classList.remove('active'));
-                divSong.classList.add('active');
-                playSongNew(song);
-            });
-        } else {
-            divSong.style.opacity = '0.6';
-            divSong.title = 'Arquivo de áudio não disponível';
-        }
+        const audio = new Audio(song.file);
 
-        library.appendChild(divSong);
+        audio.addEventListener('loadedmetadata', () => {
+            const totalSeconds = audio.duration;
+            const formatedDuration = formatSongDuration(totalSeconds);
+
+            console.log(formatedDuration);
+        
+            const divSong = document.createElement('div');
+            divSong.classList.add("songRow");
+                divSong.innerHTML = `
+                <img src="${song.cover}" alt="${song.name}">
+                <h3>${song.name}<br/></h3><p>${song.artist}</p>
+                    <p>${formatedDuration}</p>
+            `;
+        
+            if (song.file !== '#') {
+                divSong.addEventListener('click', () => {
+                    document.querySelectorAll('.songRow').forEach(i => i.classList.remove('active'));
+                    divSong.classList.add('active');
+                    playSongNew(song);
+                });
+            } else {
+                divSong.style.opacity = '0.6';
+                divSong.title = 'Arquivo de áudio não disponível';
+            }
+
+            library.appendChild(divSong);
+        });        
     });
+}
+
+function formatSongDuration(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
 
 audioPlayer.addEventListener('ended', () => {
